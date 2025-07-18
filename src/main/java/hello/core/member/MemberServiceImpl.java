32b790f2a53031147ca4b4b812c04a0af22135e1
jinlee <dlwls6877@naver.com>
2025-07-18 15:01:15 +0900

    Section3: Member domain test

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,6 +4,8 @@ public class MemberServiceImpl implements MemberService {
 
     //그냥 레포지토리만 선언하면 null이므로 구현객체를 넣어줘야함
     //memoryMemberRepository에 있는 save 호출됨
+
+    //실제 할당하는 부분이 구현체를 의존하고 있음. 따라서 구현체에도 의존하고 추상화에도 의존하고 있는 아주 안좋은 상태
     private  final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
